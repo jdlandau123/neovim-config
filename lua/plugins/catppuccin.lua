194b6306a669bb3f5643536fd40245853423c45e
jdlandau123 <jdlandau123@gmail.com>
2025-06-17 15:26:37 -0400

    add catppuccin and updated lsp config

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,17 @@
+return {
+  -- "catppuccin/nvim",
+  -- name = "catppuccin",
+  -- priority = 1000,
+  -- lazy = false,
+  -- config = function()
+  --   require("catppuccin").setup({
+  --     flavour = "auto", -- latte, frappe, macchiato, mocha
+  --     background = {
+  --         light = "latte",
+  --         dark = "macchiato",
+  --     },
+  --     no_italic = true,
+  --   })
+  --   vim.cmd.colorscheme "catppuccin"
+  -- end
+}
