cb2687941a2601b3db4d66e71146c955ab7653ef
jdlandau123 <jdlandau123@gmail.com>
2025-06-18 14:24:33 -0400

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,22 @@
+function lsp_diagnostic_message()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+  local diags = vim.diagnostic.get(bufnr, { lnum = line })
+  if #diags > 0 then
+    return diags[1].message
+  end
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      sections = {
+        lualine_c = {
+          "filename",
+          { lsp_diagnostic_message }
+        }
+      }
+    })
   end
 }
